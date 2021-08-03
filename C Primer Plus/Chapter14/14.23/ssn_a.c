11826b9abf8147959acf8a2039813cf2724bc603
m66rk9t <m66rk9t@gmail.com>
2021-08-03 10:33:28 +0800

    14.23

---
+++ C Primer Plus/Chapter14/14.23/ssn_a.c
@@ -0,0 +1,43 @@
+/* Social Security Number.c -- 打印个人社保信息*/
+#include <stdio.h>
+#include <string.h> //提供strlen()原型
+struct fullname
+{
+    char fname[10];
+    char lname[10];
+    char midname[10];
+};//结构声明，个人姓名
+struct ssn
+{
+    unsigned long int num;
+    struct fullname name;
+};//结构声明，社保信息
+void show_info(struct ssn *); //结构数组地址作参数的函数原型
+
+int main(void)
+{
+    struct ssn ssns[5] = {
+        {302039823UL, {"Flossie", "Dribble", "Mock"}},
+        {693993751UL, {"Stephen", "Prata"}},
+        {307816628UL, {"Yifan", "Li"}},
+        {793238462UL, {"James", "Jones", "Earl"}},
+        {280348253UL, {"Mark", "Hamill"}}}; //声明并初始化ssn结构数组
+
+    /*打印信息*/
+    show_info(ssns);
+    puts("Done!");
+
+    return 0;
+}
+
+/*函数定义*/
+void show_info(struct ssn * info)
+{
+    for (int i = 0; i < 5; info++, i++)
+    {
+        printf("%s, %s ", info->name.lname, info->name.fname);
+        if (strlen(info->name.midname) > 0)
+            printf("%c. ", info->name.midname[0]);
+        printf("-- %lu\n", info->num);
+    }
+}
