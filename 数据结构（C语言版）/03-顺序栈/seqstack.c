d3e70a860abe1d36fac3e0d390a9ca9e57427bc4
m66rk9t <m66rk9t@gmail.com>
2021-10-01 14:43:05 +0800

    顺序表实现

---
+++ 数据结构（C语言版）/03-顺序栈/seqstack.c
@@ -0,0 +1,108 @@
+/*顺序栈实现*/
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include "seqstack.h"
+
+bool InitStack(SeqStack *pStack)
+{
+    if (!StackIsNull(pStack))
+        DestoryStack(pStack);
+    pStack->base = (Elem *)malloc(INITSIZE * sizeof(Elem));
+    if (!pStack->base)
+        return false;
+    pStack->top = pStack->base;
+    pStack->stacksize = INITSIZE;
+
+    return true;
+}
+
+void DestoryStack(SeqStack *pStack)
+{
+    free(pStack->base);
+    pStack->base = NULL;
+}
+
+void ClearStack(SeqStack *pStack)
+{
+    //你说你有什么用啊你？？？
+}
+
+bool StackIsNull(SeqStack *pStack)
+{
+    if (pStack->base == pStack->top)
+        return true;
+    else
+        return false;
+}
+
+bool StackIsFull(SeqStack *pStack)
+{
+    if (StackLength(pStack) == pStack->stacksize)
+        return true;
+    else
+        return false;
+}
+
+unsigned int StackLength(SeqStack *pStack)
+{
+    return pStack->top - pStack->base;
+}
+
+bool Push(SeqStack *pStack, Elem *pElem)
+{
+    if (StackIsFull(pStack))
+    {
+        pStack->base = (Elem *)realloc(pStack->base, (pStack->stacksize + APPENSIZE) * sizeof(Elem));
+        if (!pStack->base)
+            return false;
+        pStack->stacksize += APPENSIZE;
+        pStack->top = pStack->base + pStack->stacksize - 1;
+    }
+    CopyDataToElem(pStack->top, pElem);
+    pStack->top++;
+
+    return true;
+}
+
+bool Pop(SeqStack *pStack)
+{
+    Elem * delElem;
+    if (StackIsNull(pStack))
+        return false;
+    delElem = pStack->top-1;
+    pStack->top--;
+    free(delElem);
+
+    return true;
+}
+
+bool GetTop(SeqStack *pStack, Elem *pElem)
+{
+    if (StackIsNull(pStack))
+        return false;
+    CopyDataToElem(pElem, pStack->top - 1);
+
+    return true;
+}
+
+void PrintElemData(Elem *pElem)
+{
+    printf("Elem:%d\n", *pElem);
+}
+
+void StackTraverse(SeqStack *pStack, void (*pFun)(Elem *))
+{
+    Elem *pElem = pStack->top - 1;
+
+    while (pElem >= pStack->base)
+    {
+        (*pFun)(pElem);
+        pElem--;
+    }
+}
+
+void CopyDataToElem(Elem *tar, Elem *src)
+{
+    *tar = *src;
+}
